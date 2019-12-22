1c16421f70a975535463be12d6775662e3fc4c6f
yjymh <814123650@qq.com>
2019-12-22 23:05:09 +0800

    up

--- mc_backups.sh
+++ mc_backups.sh
@@ -3,4 +3,4 @@ DATE=`date '+%Y-%m-%d_%H.%M.%S'`
 name="MCbackups"
 FILENAME=${DATE}${name}
 #echo $FILENAME
-tar -rPf ${FILENAME}.zip /home/ubuntu/mc/worlds/Utopia/
+tar -rPf /home/ubuntu/backups/${FILENAME}.zip /home/ubuntu/mc/worlds/Utopia/
